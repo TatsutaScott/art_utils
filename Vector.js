2dddcdb6b74138cca422a6514aa972a7966c7439
TatsutaScott <tatsuta.scott@gmail.com>
2023-08-18 16:35:18 +0900

    finished commenting vector with jsdoc style

--- Vector.js
+++ Vector.js
@@ -123,112 +123,201 @@ class Vec {
 
     return Vec(x, y);
   }
-  
-  copy() {
-    const newCopy = new Vec(this.x, this.y);
-    return newCopy;
-  }
-
-  rotate(theta) {
-    let newHeading = this.heading() + theta;
-    const mag = this.magnitude();
-    this.x = Math.cos(newHeading) * mag;
-    this.y = Math.sin(newHeading) * mag;
-    return this;
-  }
-
-  heading() {
-    const h = Math.atan2(this.y, this.x);
-    return h;
-  }
-
-  add(a, b) {
-    if (arguments.length == 1) {
-      this.x += a.x;
-      this.y += a.y;
-    }
-
-    if (arguments.length == 2) {
-      this.x += a;
-      this.y += b;
-    }
-    return this;
-  }
-
-  set(a, b) {
-    if (arguments.length == 1) {
-      this.x = a.x;
-      this.y = a.y;
-    }
-
-    if (arguments.length == 2) {
-      this.x = a;
-      this.y = b;
-    }
-    return this;
-  }
-
-  sub(a, b) {
-    if (arguments.length == 1) {
-      this.x -= a.x;
-      this.y -= a.y;
-    }
-
-    if (arguments.length == 2) {
-      this.x -= a;
-      this.y -= b;
-    }
-    return this;
-  }
-
-  mult(a, b) {
-    if (arguments.length == 1) {
-      this.x *= a;
-      this.y *= a;
-    } else if (arguments.length == 2) {
-      this.x *= a;
-      this.y *= b;
-    }
-    return this;
-  }
-
-  magnitude() {
-    const mag = Math.sqrt(this.magSq());
-    return mag;
-  }
-
-  setMag(n) {
-    return this.normalize().mult(n);
-  }
-
-  normalize() {
-    return this.limit(1);
-  }
-
-  magSq() {
-    const x = this.x;
-    const y = this.y;
-    return x * x + y * y;
-  }
-
-  limit(n) {
-    const mSq = this.magSq();
-    if (mSq > n * n) {
-      this.div(Math.sqrt(mSq)).mult(n);
-    }
-    return this;
-  }
-
-  div(n) {
-    this.x /= n;
-    this.y /= n;
-    return this;
-  }
 }
 
-const map = (num, inLo, inHi, outLo, outHi) => {
-  const inScale = (num - inLo) / (inHi - inLo);
-  const outScale = outHi - outLo;
-  return inScale * outScale + outLo;
+/**
+ * Makes a copy of the vector
+ * @returns {Vec}
+ */
+Vec.prototype.copy = function () {
+  return new Vec(this.x, this.y); // make a new vector with the x and y coordinates of the current vec
 };
+
+/**
+ * Rotates the vector by a given angle
+ * @param {number} theta - angle to rotate by (in radians)
+ * @returns {this}
+ */
+Vec.prototype.rotate = function (theta) {
+  const newHeading = this.heading() + theta; // gets the current angle and offsets it by theta
+  const magnitude = this.magnitude(); // gets the current magnitude of the vector
+  this.x = Math.cos(newHeading) * magnitude; // generates new angle's x and scales by vector's magnitude
+  this.y = Math.sin(newHeading) * magnitude; // generates new angle's y and scales by vector's magnitude
+  return this;
+};
+
+/**
+ * Calculates the angle of the vector
+ * @returns {number} the angle that the vector points
+ */
+Vec.prototype.heading = function () {
+  return Math.atan2(this.y, this.x); // calculates the heading by taking the arctangent of x and y
+};
+
+/**
+ * Adds a vector to the vector calling the method. Accepts either 1 vector argument, or 2 number arguments that serve as a vector.
+ * @param {Vec | number} a - either a vector or the x value of a vector to be added
+ * @param {number} b - y value of a vector to be added
+ * @returns {this}
+ */
+Vec.prototype.add = function (a, b) {
+  if (arguments.length === 1 && typeof a === "object") {
+    // if there is only one argument then run with a as a vector
+    this.x += a.x; // add on the x value
+    this.y += a.y; // add on the y value
+  } else if (typeof a === "number" && typeof b === "number") {
+    // if there are two number arguments run as if a and b made up a vector
+    this.x += a; // add on the x value
+    this.y += b; // add on the y value
+  } else {
+    // throw an error if the arguments don't match the expected arguments
+    throw new Error("Vec.add arguments error");
+  }
+  return this;
+};
+
+/**
+ * Subtracts a vector for the vector calling the method. Accepts either 1 vector argument, or 2 number arguments that serve as a vector.
+ * @param {Vec | number} a - either a vector or the x value of a vector to be subtracted
+ * @param {number} b - y value of a vector to be subtracted
+ * @returns {Vec}
+ */
+Vec.prototype.sub = function (a, b) {
+  if (arguments.length === 1 && typeof a === "object") {
+    // if there is only one argument then run with a as a vector
+    this.x -= a.x; // subtract x value
+    this.y -= a.y; // subtract y value
+  } else if (typeof a === "number" && typeof b === "number") {
+    // if there are two number arguments run as if a and b made up a vector
+    this.x -= a; // subtract x value
+    this.y -= b; // subtract y value
+  } else {
+    // throw an error if the arguments don't match the expected arguments
+    throw new Error("Vec.sub arguments error");
+  }
+  return this;
+};
+
+/**
+ * Multiplies the vector calling the method by another vector. Accepts either 1 vector argument, or 2 number arguments that serve as a vector.
+ * @param {Vec | number} a - either a vector or the x value of a vector to multiply by
+ * @param {number} b - y value of a vector to multiply by
+ * @returns {Vec}
+ */
+Vec.prototype.mult = function (a, b) {
+  if (arguments.length === 1 && typeof a === "object") {
+    // if there is only one argument then run with a as a vector
+    this.x *= a.x; // multiply x values
+    this.y *= a.y; // multiply y values
+  } else if (arguments.length === 1 && typeof a === "number") {
+    // if there is only one argument then run with a as a scaler value
+    this.x *= a; // multiply by a
+    this.y *= a; // multiply by a
+  } else if (typeof a === "number" && typeof b === "number") {
+    // if there are two number arguments run as if a and b made up a vector
+    this.x *= a; // multiply x values
+    this.y *= b; // multiply y values
+  } else {
+    // throw an error if the arguments don't match the expected arguments
+    throw new Error("Vec.mult arguments error");
+  }
+  return this;
+};
+
+/**
+ * Divides the Vector by a scaler value
+ * @param {number} n - scaler value to divide Vector by
+ * @returns {this}
+ */
+Vec.prototype.div = function (n) {
+  this.x /= n; // divide x value
+  this.y /= n; // divide y value
+  return this;
+};
+
+/**
+ * Sets the current vector to the incoming vector. Accepts either 1 vector argument, or 2 number arguments that serve as a vector.
+ * @param {Vec | number} a - either a vector or the x value of a vector to be set
+ * @param {number} b - y value of a vector to be set
+ * @returns {Vec}
+ */
+Vec.prototype.set = function (a, b) {
+  if (arguments.length === 1 && typeof a === "object") {
+    // if there is only one argument then run with a as a vector
+    this.x = a.x; // set x value
+    this.y = a.y; // set y value
+  } else if (typeof a === "number" && typeof b === "number") {
+    // if there are two number arguments run as if a and b made up a vector
+    this.x = a; // set x value
+    this.y = b; // set y value
+  } else {
+    // throw an error if the arguments don't match the expected arguments
+    throw new Error("Vec.set arguments error");
+  }
+  return this;
+};
+
+/**
+ * Calculates the magnitude or langth of a vector.
+ * @returns {number}
+ */
+Vec.prototype.magnitude = function () {
+  return Math.sqrt(this.magSq()); // uses the quake magsq method to calculate magnitude
+};
+
+/**
+ * Scales the vector to a given magnitude
+ * @param {number} mag - the length to set the magnitude of the vector to
+ * @returns {this}
+ */
+Vec.prototype.setMag = function (mag) {
+  return this.normalize().mult(mag); // normalizes and then multiplies to scale
+};
+
+/**
+ * Normalizes the vector so that the magnitude is 1.
+ * @returns {this}
+ */
+Vec.prototype.normalize = function () {
+  return this.limit(1); // limit the magnitude of the vector to 1
+};
+
+/**
+ * Calculate the magnitude squared of the vector
+ * @returns {number}
+ */
+Vec.prototype.magSq = function () {
+  return this.x * this.x + this.y * this.y; // squared magnitude
+};
+
+/**
+ * Limits the magnitude of the Vector to the given magnitude
+ * @param {number} mag - magnitude to limit the vector to
+ * @returns {this}
+ */
+Vec.prototype.limit = function (mag) {
+  const mSq = this.magSq(); // calculate the mag squared
+  if (mSq > mag * mag) {
+    // if the mag square of the vector is larger than that of the input magnitude limit
+    this.div(Math.sqrt(mSq)).mult(mag); //limit to the given magnitude
+  }
+  return this;
+};
+
+/**
+ * Scales a value between an input range and maps it to an output range
+ * @param {number} n - number to be scaled
+ * @param {number} inputLo - lower boundary for input range
+ * @param {number} inputHi - upper boundary for input range
+ * @param {number} outputLo - lower boundary for output range
+ * @param {number} outputHi - upper boudnary for output range
+ * @returns {number} scaled value
+ */
+
+const map = (n, inputLo, inputHi, outputLo, outputHi) => {
+  const inScale = (n - inputLo) / (inputHi - inputLo); // calculates where n sits in the input range and scales it to [0,1]
+  const outScale = outputHi - outputLo; // calculates the size of the output range
+  return inScale * outScale + outputLo; // multiplies the input ratio by the size of the output range
+};
+
 export default Vec;

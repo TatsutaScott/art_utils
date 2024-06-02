746764aeb230e5e03c4bb6c05bedd063b3ebaf03
TatsutaScott <tatsuta.scott@gmail.com>
2024-06-02 13:55:53 +0900

    added extra functions to canvas + created misc_util file

--- canvas_util.js
+++ canvas_util.js
@@ -1,5 +1,4 @@
-/**
- * Creates a canvas DOM element, as well as a 2D drawing context.
+/**Creates a canvas DOM element, as well as a 2D drawing context.
  *
  * @param {number} width Canvas width
  * @param {number} height Canvas height
@@ -8,7 +7,7 @@
  * @returns {[canvas, context]} Returns an array holding the canvas DOM element and drawing context.
  */
 
-function makeCanvas(width, height, parent = document.body) {
+export function makeCanvas(width, height, parent = document.body) {
   const canvas = document.createElement("canvas"); //Creates the canvas DOM element
   parent.appendChild(canvas); //Appends the canvas DOM element to a parent element
 
@@ -20,4 +19,38 @@ function makeCanvas(width, height, parent = document.body) {
   return [canvas, ctx];
 }
 
-export { makeCanvas };
+/**Download an image of the canvas
+ *
+ * @param {DOMElement} canvas - Canvas DOM Element
+ * @param {String} name - name for the downloaded image file
+ * @param {String} [type='png'] - file format. options: png, jpeg, webp
+ */
+export function download(canvas, name, type = "png") {
+  const link = document.createElement("a");
+  link.download = name;
+  link.href = canvas.toDataURL(`image/${type}.${type}`);
+  link.click();
+}
+
+/** sets the dimensions of the canvas
+ * @param {DOMElement} canvas - the canvas DOM object
+ * @param {number} width - width value in pixels
+ * @param {number} height - height value in pixels
+ */
+export function setDimensions(canvas, width, height) {
+  canvas.width = width;
+  canvas.height = height;
+}
+
+/**Sets the background of the canvas
+ *
+ * @param {DrawingContext} ctx - html canvas rendering context to draw to
+ * @param {String} color - color to set background to
+ */
+export function background(ctx, color) {
+  const width = ctx.canvas.width;
+  const height = ctx.canvas.height;
+
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, width, height);
+}
---
+++ misc_util.js
@@ -0,0 +1,14 @@
+/**Creates a string with the current date and time. Useful for naming downloads to keep track of time
+ *
+ * @returns {String} - String with date "Year.Month.Day_Hour.Minute"
+ */
+export function dateString() {
+  const now = new Date();
+
+  const date_string = `${now.getFullYear()}.${
+    now.getMonth() + 1
+  }.${now.getDate()}`;
+  const time_string = `${now.getHours()}.${now.getMinutes()}`;
+
+  return date_string + "_" + time_string;
+}

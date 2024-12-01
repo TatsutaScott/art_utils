2020d5291377bcd32fffb088aa48847bf71b13d1
TatsutaScott <tatsuta.scott@gmail.com>
2024-11-30 20:11:09 -0800

    added documentation

--- CubeHelix.js
+++ CubeHelix.js
@@ -1,8 +1,19 @@
-import { map, limit } from "./math_util.js";
+import { limit } from "./math_util.js";
 import { random } from "./random_util.js";
 import Color from "./Color.js";
 
+/**
+ * A class for generating colors based on the CubeHelix color scheme.
+ */
 class CubeHelix {
+  /**
+   * Creates a CubeHelix instance.
+   *
+   * @param {number} start - Starting value for the color scheme, ranging from 0 to 3.
+   * @param {number} rot - The number of rotations through the color space, ranging from -1.5 to 1.5.
+   * @param {number} hue - Hue intensity factor, ranging from 0 to 4.
+   * @param {number} gamma - Gamma correction factor, ranging from 0.6 to 1.4.
+   */
   constructor(start, rot, hue, gamma) {
     this.start = start; // 0 <-> 3
     this.rotation = rot; // -1.5 <-> 1.5
@@ -11,6 +22,11 @@ class CubeHelix {
     this.print();
   }
 
+  /**
+   * Generates a random CubeHelix instance with random parameters within valid ranges.
+   *
+   * @returns {CubeHelix} A new CubeHelix instance with random parameters.
+   */
   static random() {
     const start = random(0, 3);
     const rot = random(-1.5, 1.5);
@@ -21,6 +37,12 @@ class CubeHelix {
   }
 }
 
+/**
+ * Computes a single color in the CubeHelix palette based on a normalized input.
+ *
+ * @param {number} n - A normalized value (0 to 1) representing the position in the palette.
+ * @returns {Color} A `Color` object representing the computed RGB color.
+ */
 CubeHelix.prototype.get = function (n) {
   const angle = 2 * Math.PI * (this.start / 3.0 + 1 + this.rotation * n);
   const fraction = Math.pow(n, this.gamma);
@@ -40,6 +62,12 @@ CubeHelix.prototype.get = function (n) {
   return new Color(r, g, b);
 };
 
+/**
+ * Generates a palette of colors based on the CubeHelix algorithm.
+ *
+ * @param {number} divisions - The number of colors to generate in the palette.
+ * @returns {Color[]} An array of `Color` objects representing the generated palette.
+ */
 CubeHelix.prototype.palette = function (divisions) {
   const palette = [];
   for (let i = 0; i <= divisions - 1; i++) {
@@ -49,6 +77,9 @@ CubeHelix.prototype.palette = function (divisions) {
   return palette;
 };
 
+/**
+ * Logs the CubeHelix instance's parameters to the console.
+ */
 CubeHelix.prototype.print = function () {
   console.log(
     `Cube Helix: 
@@ -58,4 +89,5 @@ CubeHelix.prototype.print = function () {
     gamma: ${this.gamma}`
   );
 };
+
 export default CubeHelix;
